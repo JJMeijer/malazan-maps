431388ae45e420c0208b4bc912ee0685e53a47bd
JJMeijer <18067438+JJMeijer@users.noreply.github.com>
2021-05-18 21:42:19 +0200

    start search engine

--- static/js/base.js
+++ static/js/base.js
@@ -1,14 +1,29 @@
 (function wrapper() {
+    const createSearchResult = (name, type, link) => {
+        const resultWrapper = document.createElement('div');
+
+        const resultLink = document.createElement('a');
+        resultLink.href = link;
+
+        const resultName = document.createElement('p');
+        resultName.innerHTML = name;
+
+        const resultType = document.createElement('p');
+        resultType.innerHTML = type;
+
+        resultLink.appendChild(resultName);
+        resultLink.appendChild(resultType);
+        resultWrapper.appendChild(resultLink);
+
+        console.log(resultWrapper);
+    };
+
     const setupSearch = () => {
         const searchResultsBox = document.getElementById('search-results');
         const searchBox = document.getElementById('search');
 
-        searchBox.addEventListener('focus', () => {
-            searchResultsBox.classList.remove('hidden');
-        });
-
-        searchBox.addEventListener('blur', () => {
-            searchResultsBox.classList.add('hidden');
+        searchBox.addEventListener('input', (event) => {
+            //
         });
     };
 
