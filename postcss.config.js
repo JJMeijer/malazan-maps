e2c18696f1c43663aa6d034b0d6315ef9ff16a00
JJMeijer <18067438+JJMeijer@users.noreply.github.com>
2021-09-15 15:20:33 +0200

    added map & fix css link

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,7 @@
 /* eslint-env node */
-
 module.exports = {
-    plugins: [require('tailwindcss'), require('autoprefixer')],
+    plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+    },
 };
