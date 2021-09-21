0dee063c44503979ad0684c86434e3df13bdf482
JJMeijer <18067438+JJMeijer@users.noreply.github.com>
2021-09-21 20:24:50 +0200

    fix css build bug

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@
 const plugin = require('tailwindcss/plugin');
 
 module.exports = {
-    purge: ['./mapsdata/templates/*.html', './mapsdata/static/js/*.js'],
+    purge: ['./mapsdata/templates/**/*.html', './mapsdata/static/js/*.js'],
     darkMode: false, // or 'media' or 'class'
     theme: {
         extend: {
