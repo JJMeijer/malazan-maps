6c827165932ec572fce5f368e0f987a8f8e6e370
JJMeijer <18067438+JJMeijer@users.noreply.github.com>
2021-09-25 19:53:57 +0200

    add canonical

--- mapsdata/processor.py
+++ mapsdata/processor.py
@@ -5,6 +5,7 @@ from mapsdata.serializers import serialize_all
 def common_context(request):
     """Common Context that should be used in all views"""
     return {
+        "request_path": request.path,
         "debug_flag": settings.DEBUG,
         "entries": serialize_all()
     }
