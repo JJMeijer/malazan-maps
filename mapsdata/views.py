ae7e175a9e3f3cc2d2008bdd60f0d1a6901ba529
JJMeijer <18067438+JJMeijer@users.noreply.github.com>
2021-09-28 14:19:00 +0200

    portrait image on homepage

--- mapsdata/views.py
+++ mapsdata/views.py
@@ -6,14 +6,17 @@ from mapsdata.models import Book, Continent, Place, Map
 
 def home_view(request):
     """Homepage View"""
-    header_map = Map.objects.get(name='Northwest Genabackis')
+    header_map_landscape = Map.objects.get(name='Northwest Genabackis')
+    header_map_portrait = Map.objects.get(name="Genabackis (GotM)")
+
     context = {
         'page_title': 'Home',
         'page_description': 'Malazan Maps Search Engine. Find all cities, regions & continents' \
                             ' mentioned in the books of the "Malazan: Book of the Fallen series"' \
                             ' as well as in the other novels from the Malazan world.',
         'description': 'Find places in the world of Malazan: Book of the Fallen.',
-        'header_map': header_map
+        'header_map_landscape': header_map_landscape,
+        'header_map_portrait': header_map_portrait
     }
 
     return render(request, 'home.html', context)
