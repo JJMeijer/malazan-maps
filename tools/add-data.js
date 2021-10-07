542061f9d935fad94c8c8c443cc96c1a3316dbd3
JJMeijer <18067438+JJMeijer@users.noreply.github.com>
2021-10-07 09:31:29 +0200

    Add way to add data

--- tools/add-data.js
+++ tools/add-data.js
@@ -1,7 +1,14 @@
+/* eslint-disable prettier/prettier */
 const prompts = require('prompts');
 const slugify = require('slugify');
+const fs = require('fs');
+
 const { wikiSearch, wikiSummary, wikiUrl } = require('./wiki');
+const { validateContent } = require('./validate');
+
 const maps = require('./maps.json');
+const content = require('../views/_data/content.json');
+
 
 (async () => {
     const response = await prompts([
@@ -61,12 +68,40 @@ const maps = require('./maps.json');
         y: markerY,
     };
 
-    console.log({
+    const newItem = {
         name,
         slug,
         description,
         wikiLink,
         type,
         maps: [map],
-    });
+    };
+
+    const existingItemIndex = content.findIndex((item) => item.name === name);
+    const isNew = existingItemIndex === -1;
+
+    if (isNew) {
+        content.push(newItem);
+        content.sort((a, b) => a.name.localeCompare(b.name));
+
+        validateContent(content);
+
+        fs.writeFileSync(__dirname + '/../views/_data/content.json', JSON.stringify(content, null, 4));
+        console.log(`[${__filename}] Added new item: ${newItem.name}`);
+        return;
+    }
+
+    const existingMap = content[existingItemIndex].maps.findIndex(
+        (map) => map.name === newItem.maps[0].name,
+    );
+
+    if (existingMap) {
+        console.log(`[${__filename}] Item With provided map already exists`);
+        return;
+    }
+
+    content[existingItemIndex].maps.concat(newItem.maps);
+    validateContent();
+    fs.writeFileSync(__dirname + '/../views/_data/content.json', JSON.stringify(content, null, 4));
+    console.log(`[${__filename}] Added map "${newItem.maps[0].name}" to existing Content Item "${newItem.name}".`);
 })();
