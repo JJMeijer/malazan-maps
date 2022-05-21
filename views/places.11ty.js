885cc07f8e12f9ed111fe69ec7ec0312cd21a582
JJMeijer <18067438+JJMeijer@users.noreply.github.com>
2022-05-21 21:50:09 +0200

    add places data for future

---
+++ views/places.11ty.js
@@ -0,0 +1,48 @@
+/**
+ * Return all places registered for a certain map. Currently not in use, but can be used
+ * for map pages to display relevant place information on hover or tap.
+ */
+const slugify = require("slugify");
+
+class Places {
+    data() {
+        return {
+            pagination: {
+                data: "maps",
+                size: 1,
+                alias: "map",
+            },
+            permalink: ({ map: { name } }) => `map/${slugify(name)}/places.json`,
+        };
+    }
+
+    render(data) {
+        const {
+            content,
+            pagination: { pageNumber: mapIndex },
+        } = data;
+
+        const places = content.filter((place) => {
+            return place.maps.find((x) => x.id === mapIndex && x.marker);
+        });
+
+        const mappedPlaces = places.map((place) => {
+            const { name, slug, description, type, wikiLink, maps } = place;
+
+            const marker = maps.find((x) => x.id === mapIndex).marker;
+
+            return {
+                name,
+                slug,
+                description,
+                type,
+                wikiLink,
+                marker,
+            };
+        });
+
+        return JSON.stringify(mappedPlaces);
+    }
+}
+
+module.exports = Places;
